04667bcb191b42062b8ef2f8ff57513ca4f9f8ee
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2014-10-18 17:55:34 -0500

    Better title formatting

--- src/rettyt/cli.py
+++ src/rettyt/cli.py
@@ -17,19 +17,33 @@ import curses
 import praw
 import webbrowser
 
-def draw_submissions(win, posts):
-    (lines, cols) = win.getmaxyx()
+top_line = None
+body = None
+bottom_line = None
+
+def submission_to_string(submission, limit):
+    left = "↑ {} ".format(submission.score)
+    right = " ({}) [/r/{}]".format(submission.domain,
+                                   submission.subreddit.display_name)
+    rawTitle = submission.title
+    rawTitle = rawTitle.replace("&amp;", "&")
+    rawTitle = rawTitle.replace("&lt;", "<")
+    rawTitle = rawTitle.replace("&gt;", ">")
+    titlelen = min(len(rawTitle), limit - len(left) - len(right) - 3)
+    title = rawTitle[0:titlelen]
+    if len(title) < len(rawTitle):
+        title += "..."
+    return left + title + right
+
+def draw_submissions(posts):
+    global body
+    (lines, cols) = body.getmaxyx()
     pos = 0
     for entry in posts:
-        hpos = 0
-        win.addstr(pos, hpos, "↑ ")
-        hpos += 2
-        ups = str(entry.ups)
-        win.addstr(pos, hpos, ups)
-        hpos += len(ups) + 1
-        win.addnstr(pos, hpos, entry.title, cols - hpos - 1)
+        post_str = submission_to_string(entry, cols - 1)
+        body.addstr(pos, 0, post_str)
         pos += 1
-    win.refresh()
+    body.refresh()
 
 def paint_line(window, line):
     window.chgat(line, 0, curses.color_pair(2))
@@ -50,6 +64,7 @@ def grab_screenful(submissions, limit):
     return
 
 def curses_main(stdscr):
+    global top_line, bottom_line, body
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
     stdscr.refresh()
@@ -58,7 +73,7 @@ def curses_main(stdscr):
 
     top_line = curses.newwin(1, cols, 0, 0)
     bottom_line = curses.newwin(1, cols, lines - 1, 0)
-    remainder = curses.newwin(lines - 2, cols, 1, 0)
+    body = curses.newwin(lines - 2, cols, 1, 0)
 
     top_line.bkgd(ord(' '), curses.color_pair(1))
     bottom_line.bkgd(ord(' '), curses.color_pair(1))
@@ -73,39 +88,39 @@ def curses_main(stdscr):
     frontpage = next(frontpages)
     page_num = 1
     current_entry = 0
-    draw_submissions(remainder, frontpage)
-    paint_line(remainder, 0)
+    draw_submissions(frontpage)
+    paint_line(body, 0)
     while True:
         key = stdscr.getch()
         if key == ord('q') or key == ord('Q') or key == 3:
             return
         elif key == (curses.KEY_UP) or key == ord('k'):
             if current_entry > 0:
-                unpaint_line(remainder, current_entry)
+                unpaint_line(body, current_entry)
                 current_entry -= 1
-                paint_line(remainder, current_entry)
+                paint_line(body, current_entry)
         elif key == curses.KEY_DOWN or key == ord('j'):
             if current_entry < len(frontpage) - 1:
-                unpaint_line(remainder, current_entry)
+                unpaint_line(body, current_entry)
                 current_entry += 1
-                paint_line(remainder, current_entry)
+                paint_line(body, current_entry)
         elif key == ord(' ') and len(frontpage) != 0:
             frontpage = next(frontpages)
             page_num += 1
             current_entry = 0
-            remainder.clear()
-            draw_submissions(remainder, frontpage)
-            paint_line(remainder, 0)
+            body.clear()
+            draw_submissions(frontpage)
+            paint_line(body, 0)
             bottom_line.clear()
             bottom_line.addstr("Front page ({})".format(page_num))
             bottom_line.refresh()
         elif key == ord('\n'):
             webbrowser.open_new_tab(frontpage[current_entry].url)
         elif key == ord('r'):
-            remainder.clear()
-            draw_submissions(remainder, frontpage)
-            paint_line(remainder, current_entry)
-        remainder.refresh()
+            body.clear()
+            draw_submissions(frontpage)
+            paint_line(body, current_entry)
+        body.refresh()
 
 def main():
     curses.wrapper(curses_main)
