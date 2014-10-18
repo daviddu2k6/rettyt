bb9a6e3339fd7016762d7b54520482b407656c1f
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2014-10-18 12:27:09 -0500

    Hello, World

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "rettyt",
+    version = "0.0.1",
+    author = "Krzysztof Drewniak et. al.",
+    author_email = "krzysdrewniak@gmail.com",
+    description = "Command-line reddit client",
+    license = "GPL",
+    package_dir = {'': 'src'},
+    packages = ["rettyt"],
+    long_description = read("README"),
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console :: Curses",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    entry_points = {
+        'console_scripts' : ['rettyt=rettyt.cli:main']
+    }
+)
